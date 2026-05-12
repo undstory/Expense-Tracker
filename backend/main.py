1fa0d55b451538d4e63b2e4818ad4a372e6f6f06
undstory <undrul.ozog@gmail.com>
2026-05-12 19:20:37 +0200

    fix: add vercel origin to CORS policy

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(lifespan=lifespan,title="Daily Expense Tracker API")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://expense-tracker-kappa-black-65.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://expense-tracker-kappa-black-65.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
